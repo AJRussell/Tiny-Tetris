0b60d7ad58c6e98720252d99db3245903238ddc9
AJRussell <AJRussell@users.noreply.github.com>
2016-09-18 13:43:14 +0100

    Update TinyTetris.ino

--- TinyTetris.ino
+++ TinyTetris.ino
@@ -31,20 +31,20 @@ Create defines for all the magic numbers but they are useful for now.
 
 
 #define OLED_ADDRESS	        	0x3C //you may need to change this, this is the OLED I2C address.  
-#define OLED_COMMAND	            0x80
+#define OLED_COMMAND	                0x80
 #define OLED_DATA	                0x40
 #define OLED_DISPLAY_OFF	        0xAE
-#define OLED_DISPLAY_ON	            0xAF
+#define OLED_DISPLAY_ON	                0xAF
 #define OLED_NORMAL_DISPLAY	    	0xA6
 #define OLED_INVERSE_DISPLAY     	0xA7
 #define OLED_SET_BRIGHTNESS	        0x81
 #define OLED_SET_ADDRESSING	        0x20
 #define OLED_HORIZONTAL_ADDRESSING	0x00 
 #define OLED_VERTICAL_ADDRESSING	0x01
-#define OLED_PAGE_ADDRESSING	    0x02 
+#define OLED_PAGE_ADDRESSING	        0x02 
 
-#define OLED_SET_COLUMN             0x21
-#define OLED_SET_PAGE	            0x22
+#define OLED_SET_COLUMN                 0x21
+#define OLED_SET_PAGE	                0x22
 
 
 #define KEYPAD_PIN	                A0

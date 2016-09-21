0506a087f7b9579a98cf54c7080f891e3cdf9b01
AJRussell <AJRussell@users.noreply.github.com>
2016-09-21 20:49:53 +0100

    Update TinyTetris.ino

--- TinyTetris.ino
+++ TinyTetris.ino
@@ -1,10 +1,17 @@
 
-/*  Tiny Tetris by Anthony Russell 17-09-2016*/
+/*  Tiny Tetris 
 
-/*  V0.91                                     */
+ V0.91                                     */
+
+Copyright (C) 2016 Anthony Russell
 
 
-/*
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
 
 To do:
 
